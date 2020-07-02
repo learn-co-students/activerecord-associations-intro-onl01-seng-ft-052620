4af4fa604465decc5697d98194fb841c98f2cf3e
francepants <franceline90@gmail.com>
2020-07-01 22:44:25 -0500

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,6 @@
 class Genre < ActiveRecord::Base
+    # A genre can have many songs. Let's implement that with the has_many macro:
+    has_many :songs
+    # A genre also has many artists through its songs. Let's implement this relationship with the has_many through macro:
+    has_many :artists, through: :songs
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ActiveRecord::Base
+    # We need to tell the Song class that it will produce objects that can belong to an artist. We will do it with the belongs_to macro:
+    belongs_to :artist
+    # Songs also belong to a genre, so we'll use the same macro to implement that relationship:
+    belongs_to :genre
 
 end
